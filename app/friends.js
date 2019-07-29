08cebaf06d65f8d0536525840a5f84a03a4c4af6
E Dubby Dubs <46727008+E-DubbyDubs@users.noreply.github.com>
2019-07-29 01:13:02 -0400

    ally oop

---
+++ app/friends.js
@@ -0,0 +1,52 @@
+var userData = [
+    {
+      "name": "Rachel Green",
+      "photo": "images/rachel"
+      "scores":[
+          5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+      ]
+    },
+    {
+      "name": "Monica Geller",
+      "photo": "images/monica",
+      "scores":[
+          3,
+          2,
+          2,
+          4,
+          1,
+          1,
+          5,
+          1,
+          3,
+          1
+      ]
+    },
+    {
+      "name": "Phoebe Buffay",
+      "photo": "images/phoebe",
+      "scores":[
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+      ]
+    }
+  ];
+  
+  module.exports = userData;
